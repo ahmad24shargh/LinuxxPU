a73e75d6d0198fac9ea64e4b0390d4ab0179b41e
ahmad24shargh <myitrayan@gmail.com>
2025-06-27 15:41:07 +0330

    Update calc_patch_tg.sh

--- .github/scripts/calc_patch_tg.sh
+++ .github/scripts/calc_patch_tg.sh
@@ -6,6 +6,10 @@ echo "inside $0"
 echo "C_TAG_NAME=${C_TAG_NAME}"
 echo "C_LAST_COMMIT=${C_LAST_COMMIT}"
 echo "C_VERSION=${C_VERSION}"
+if [ -f ${GITHUB_WORKSPACE}/TAG_NAME.ME ];then
+  for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'let version_name' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -E -i 's|(let version_name = )version_name(\.trim.+)|\1"'$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME)'"\2|g' $file;cat $file | grep  'let version_';done
+  for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'return getGitDescribe()' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -i 's|return getGitDescribe()|return "'$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME)'"|g' $file;cat $file | grep  'return';done
+fi
 #for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'let version_code' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -E -i 's|(let version_code)(.*=.+\+.+)version_code(.+)|\1\2'${C_VERSION}'\3|g' $file;done
 #for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'let version_name' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -E -i 's|(let version_name = )version_name(\.trim.+)|\1"'${C_TAG_NAME}'"\2|g' $file;cat $file | grep  'let version_';done
 #for file in $(grep -RE --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'KSU_(GIT|LOCAL|GITHUB)_VERSION' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -E -i 's|(KSU_[^[:space:]]+_VERSION \:=).+|\1 '${C_VERSION}'|g' $file;cat $file | grep -E 'KSU_(GIT|LOCAL|GITHUB)_VERSION';done
