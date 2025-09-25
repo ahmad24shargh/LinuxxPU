380cb71c8c0332fcf4700e42615269f916771cf7
ahmad24shargh <myitrayan@gmail.com>
2025-09-26 01:44:22 +0330

    Update calc_patch_tg.sh

--- .github/scripts/calc_patch_tg.sh
+++ .github/scripts/calc_patch_tg.sh
@@ -3,9 +3,6 @@ echo "inside $0"
 #C_VERSION=$1
 #C_LAST_COMMIT=$2
 #C_TAG_NAME=$3
-echo "C_TAG_NAME=${C_TAG_NAME}"
-echo "C_LAST_COMMIT=${C_LAST_COMMIT}"
-echo "C_VERSION=${C_VERSION}"
 if [ -f ${GITHUB_WORKSPACE}/TAG_NAME.ME ];then
   for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'let version_name' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -E -i 's/(let version_name = )version_name(\.trim.+)/\1"'$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d)'"\2/g' $file;cat $file | grep  'let version_';done
   for file in $(grep -R --exclude='.github' --exclude='.git' $GITHUB_WORKSPACE -e 'return getGitDescribe()' 2>/dev/null | cut -d':' -f 1 | sort | uniq);do [ "$file" != "$0" ] && sed -i 's/return getGitDescribe()/return "'$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d)'"/g' $file;cat $file | grep  'return';done
@@ -14,16 +11,7 @@ if [ -f ${GITHUB_WORKSPACE}/TAG_NAME.ME ];then
     # S1NVTg== = KSUN
     # U1VLSVNV = SUKISU
     echo $ksu_fork
-    if [ "${ksu_fork}" == 'KSUN' ];then
-      tag_name=$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d)
-    elif [ "${ksu_fork}" == 'SUKISU' ];then
-      tag_name=$(echo $(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d) | cut -d'-' -f1)
-    elif [ "${ksu_fork}" == 'xxKSU' ];then
-      tag_name=$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d)
-    else
-      exit 1
-      false
-    fi
+    tag_name=$(cat ${GITHUB_WORKSPACE}/TAG_NAME.ME | base64 -d)
     sed -E -i 's|val managerVersionName by extra.+|val managerVersionName by extra("'${tag_name}'")|g' "${GITHUB_WORKSPACE}/manager/build.gradle.kts"
   fi
 fi
